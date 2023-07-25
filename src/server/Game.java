d44662497f5f25c5a17c1937649d29335e7810f1
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2023-07-25 15:10:49 -0700

    add game class methods

--- src/server/Game.java
+++ src/server/Game.java
@@ -17,7 +17,7 @@ public class Game
     public static final int BOARD_COLS = 18;
     public static final int BOARD_TOTAL = BOARD_ROWS * BOARD_COLS;
     private int free = BOARD_TOTAL;
-    private int[][] board;
+    private int[][] gameboard;
 
     // Player configurations
     private int playerCount = 0;
@@ -45,14 +45,74 @@ public class Game
     // Initialize board data
     private void initializeBoard()
     {
-        board = new int[BOARD_ROWS][BOARD_COLS];
+        gameboard = new int[BOARD_ROWS][BOARD_COLS];
         for(int i = 0; i < BOARD_ROWS; i++)
         {
             for(int j = 0; j < BOARD_COLS; j++)
             {
-                board[i][j] = 0;
+                gameboard[i][j] = 0;
             }
         }
+    }
+
+    // Add a player given playerID and returns the total number of players in game
+    public int addPlayer(int playerID)
+    {
+        playerCount++;
+        playerScores.add(playerID, 0);
+        return playerCount;
+    }
+
+    // Check if a player can draw on square
+    // Returns 1 if player can draw, 0 if the square is already occupied, 
+    // -1 if square is being drawn on by another player
+    public int canDraw(int row, int column)
+    {
+        if(gameboard[row][column] == 0) 
+            return 1; 
+        else if(gameboard[row][column] < 0) 
+            return -1;
+        else 
+            return 0;
+    }
+
+    // Set a square to be drawn on by a player
+    public void startDrawing(int row, int column, int playerID)
+    {
+        gameboard[row][column] = -playerID;
+    }
+
+    // Claim a square for a player, returns true if successful, false if the square invalid
+    public boolean claimSquare(int row, int column, int playerID)
+    {
+        if(gameboard[row][column] == -playerID)
+        {
+            gameboard[row][column] = playerID;
+            playerScores.set(playerID, playerScores.get(playerID) + 1);
+            this.free--;
+            return true;
+        }
+        return false;
+    }
+
+    // Check if a player has won the game, returns the playerID if they have, -1 if not
+    public int checkWin()
+    {
+        int playerID = -1;
+        int max = 0;
+
+        if(this.free == 0)
+        {
+            for(int i = 0; i < this.playerCount; i++)
+            {
+                if(playerScores.get(i) > max)
+                {
+                    max = playerScores.get(i);
+                    playerID = i;
+                }
+            }
+        }
+        return playerID;
     }
 
     // Debug: Print current gameboard state
@@ -67,9 +127,9 @@ public class Game
             System.out.print("| ");
             for(int j = 0; j < BOARD_COLS; j++)
             {
-                if(board[i][j] == 0) { free++; }
+                if(gameboard[i][j] == 0) { free++; }
                 else { occupied++; }
-                System.out.print(board[i][j] + " ");
+                System.out.print(gameboard[i][j] + " ");
             }
             System.out.println("|");
         }
