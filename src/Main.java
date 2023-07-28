f3e7ac2302c7cb17146d3bdf2abacd5e07c6a68c
Gurkirat Singh <101526938+goki-1@users.noreply.github.com>
2023-07-27 23:18:52 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,12 @@ public class Main {
     public static final int PORT = 5000;
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("CMPT 371 Project");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Screen s = new Screen();
-        frame.add(s);
-        frame.pack();
-        frame.setVisible(true);
+		// JFrame frame = new JFrame("CMPT 371 Project");
+  //       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  //       Screen s = new Screen();
+  //       frame.add(s);
+  //       frame.pack();
+  //       frame.setVisible(true);
 
         new TCPServer(PORT).startServer();
 	}
