17bea6c3b6c798020b496cb9600807d6e95d931f
EJ <ejnicolas001@gmail.com>
2023-08-01 19:49:08 -0700

    Update mouse movement to fit in screen and smoother

--- src/Screen.java
+++ src/Screen.java
@@ -10,12 +10,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 import classes.*;
 
-public class Screen extends JPanel implements ActionListener, MouseListener {
+public class Screen extends JPanel implements ActionListener, MouseListener, MouseMotionListener {
 	private Timer timer;
 	public final static int SCREEN_WIDTH = 1600;
 	public final static int SCREEN_HEIGHT= 900;
@@ -39,6 +41,7 @@ public class Screen extends JPanel implements ActionListener, MouseListener {
 		playerCount = clientID;
 		timer = new Timer(5,this);
 		addMouseListener(this);
+		addMouseMotionListener(this);
 		setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
 		this.setLayout(null);
 		
@@ -53,26 +56,6 @@ public class Screen extends JPanel implements ActionListener, MouseListener {
 	//method that runs according to the timer. Practically keeps track of the mouse movements
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		PointerInfo pointerInfo = MouseInfo.getPointerInfo();
-		if (pointerInfo != null) {
-			Point mouseLocation = pointerInfo.getLocation();
-			mousePosX = (int) mouseLocation.getX();
-			mousePosY = (int) mouseLocation.getY();
-		}
-	
-		if (isDrawing) {
-			if (currSquare != null) {
-				currSquare.addDot(new Dot(localPlayer, mousePosX, mousePosY));
-			}
-			
-			//if the player's mouse moves out of the current box, check if they can claim the square
-			if(currSquare != findCurrSquare(mousePosX, mousePosY)) {	
-				if(currSquare.checkDotsArea()) currSquare.lockSquare(localPlayer);
-				else currSquare.unlockSquare();
-			}
-		}
-	
-		localPlayer.setPosition(mousePosX, mousePosY);
 	
 		//draw screen
 		repaint();
@@ -111,6 +94,25 @@ public class Screen extends JPanel implements ActionListener, MouseListener {
 		return null;
 	}
 	
+	void trackMouse(MouseEvent e) {
+		mousePosX = e.getX();
+		mousePosY = e.getY();
+	
+		if (isDrawing) {
+			if (currSquare != null) {
+				currSquare.addDot(new Dot(localPlayer, mousePosX, mousePosY));
+			}
+			
+			//if the player's mouse moves out of the current box, check if they can claim the square
+			if(currSquare != findCurrSquare(mousePosX, mousePosY)) {	
+				if(currSquare.checkDotsArea()) currSquare.lockSquare(localPlayer);
+				else currSquare.unlockSquare();
+			}
+		}
+	
+		localPlayer.setPosition(mousePosX, mousePosY);
+	}
+	
 	//Callback events for mouse listener
 	@Override
 	public void mouseClicked(MouseEvent e) {}
@@ -138,12 +140,26 @@ public class Screen extends JPanel implements ActionListener, MouseListener {
 			else currSquare.unlockSquare();
 		}
 	}
+	
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		trackMouse(e);
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		trackMouse(e);
+	}
+	
 
 	@Override
 	public void mouseEntered(MouseEvent e) {}
 
 	@Override
 	public void mouseExited(MouseEvent e) {}
+
+
+
 	
 	
 
